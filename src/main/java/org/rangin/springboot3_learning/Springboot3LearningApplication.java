ee275d9b0ec256a565e44705851bc66c77864ecd
amanmrangin <129334520+amanmrangin@users.noreply.github.com>
2023-03-30 09:52:12 -0700

    Corrected the project name

--- src/main/java/org/rangin/springboot3_learning/Springboot3LearningApplication.java
+++ src/main/java/org/rangin/springboot3_learning/Springboot3LearningApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootLearningApplication {
+public class Springboot3LearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootLearningApplication.class, args);
+		SpringApplication.run(Springboot3LearningApplication.class, args);
 	}
 
 }
